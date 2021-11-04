d225a1b36350fb903dcbfa85d9e9d6a05cc3c6f1
danembb <dbrophy720@gmail.com>
2021-11-04 08:53:32 -0600

    Create holiday service test and update base service

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,9 +1,11 @@
 class BaseService
-  def parse_json(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
+  class << self
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
 
-  def conn(url)
-    Faraday.new(url)
+    def conn(url)
+      Faraday.new(url)
+    end
   end
 end
---
+++ spec/services/holiday_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'holiday service' do
+  it 'returns holidays for the next year given a country' do
+    response = HolidayService.get_yearly_holidays("US")
+
+    expect(response).to be_an(Array)
+    expect(response.first).to be_a(Hash)
+    expect(response.first).to have_key(:date)
+    expect(response.first).to have_key(:name)
+  end
+end
