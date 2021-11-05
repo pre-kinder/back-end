2e669809ec0092a472d601587bf3409f749763ae
danembb <dbrophy720@gmail.com>
2021-11-04 18:04:22 -0600

    Refactor holiday controller with conditional logic

--- app/controllers/api/v1/holidays_controller.rb
+++ app/controllers/api/v1/holidays_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::HolidaysController < ApplicationController
 
   def index
-    @holidays = HolidayFacade.next_three_holidays("US")
-    json_response(HolidaySerializer.new(@holidays))
-    # render json:HolidaySerializer.new(@holidays)
+    holidays = HolidayFacade.next_three_holidays("US")
+    if holidays.nil?
+      json_response({ error: "Not Found" }, :not_found)
+    else
+      json_response(HolidaySerializer.new(holidays))
+      # render json:HolidaySerializer.new(@holidays)
+    end
   end
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
-  include Response
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
 end
