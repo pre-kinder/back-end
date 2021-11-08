abb0695fd2a8115d1a848fc21b5a775f0795b77d
danembb <dbrophy720@gmail.com>
2021-11-08 00:27:16 -0700

    Create sad path testing for index request spec

--- spec/requests/api/v1/teachers/index_request_spec.rb
+++ spec/requests/api/v1/teachers/index_request_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Teachers index endpoint' do
   it 'returns a list of teachers in a classroom' do
-
     classroom = create(:classroom, :with_teachers)
 
     get "/api/v1/classrooms/#{classroom.id}/teachers"
@@ -53,5 +52,21 @@ RSpec.describe 'Teachers index endpoint' do
     expect(teachers).to have_key(:data)
     expect(teachers[:data]).to be_an(Array)
     expect(teachers[:data]).to eq([])
+  end
+
+  it 'only returns teachers within a given classroom' do
+    classroom = create(:classroom, :with_teachers)
+    classroom_2 = create(:classroom, :with_teachers)
+    teacher_1 = classroom.teachers.first
+    teacher_2 = classroom_2.teachers.first
+
+    get "/api/v1/classrooms/#{classroom.id}/teachers"
+
+    expect(response).to be_successful
+
+    teachers = JSON.parse(response.body, symbolize_names: true)
+
+    expect(teachers[:data].first[:attributes][:first_name]).to eq(teacher_1.first_name)
+    expect(teachers[:data].first[:attributes][:first_name]).to_not eq(teacher_2.first_name)
   end
 end
