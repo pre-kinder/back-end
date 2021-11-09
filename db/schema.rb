eb6e34939e4443ddaaba194f2f3e115bf06aa563
kevn7n <kevn_nguyen@yahoo.com>
2021-11-09 01:37:17 -0700

    Update event bot with faker

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,18 @@
-ActiveRecord::Schema.define(version: 2021_11_05_211819) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2021_11_09_081953) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "children", force: :cascade do |t|
@@ -28,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_11_05_211819) do
     t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["classroom_id"], name: "index_events_on_classroom_id"
   end
 
