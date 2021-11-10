e06357bc74a6bc5148c4788261bdc75dc3db55db
iPowers <isika.powers@gmail.com>
2021-11-10 10:55:16 -0700

    Remove attendance status for children table.

--- db/migrate/20211109213831_remove_attendance_from_children.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAttendanceFromChildren < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :children, :attendance_status
-  end
-
-  def down
-    add_column :children, :attendance_status, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_213831) do
+ActiveRecord::Schema.define(version: 2021_11_09_184748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,11 +46,11 @@ ActiveRecord::Schema.define(version: 2021_11_09_213831) do
   create_table "events", force: :cascade do |t|
     t.bigint "classroom_id"
     t.string "title"
+    t.text "description"
     t.date "date"
     t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
     t.index ["classroom_id"], name: "index_events_on_classroom_id"
   end
 
