1bd067907f5b5514125fd491d684e812e79b71ee
iPowers <isika.powers@gmail.com>
2021-11-11 10:31:33 -0700

    Create query params to find by email and google_id.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.search_email(email_params)
+    where('email = ?', email_params)
+  end
+
+  def self.search_google_id(google_id_params)
+    where('google_id = ?', google_id_params)
+  end
 end
