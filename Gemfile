926cf1774f157672f593da674f6e0f2cdda5aa48
iPowers <isika.powers@gmail.com>
2021-11-11 14:44:51 -0700

    Add factory_bot_rails

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'json'
 gem 'jsonapi-serializer'
 gem 'figaro'
 gem 'pusher'
+gem 'factory_bot_rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,11 @@ Event.destroy_all
 Classroom.destroy_all
 Parent.destroy_all
 
-class1 = create(:classroom)
-class2 = create(:classroom)
-class3 = create(:classroom)
+
+class1 = create(name: "Zoom 1 Pintozzi Pumas")
+class2 = create(name: "Room 2 Chipmunks")
+class3 = create(name: "Room 3 Frogs")
+
 
 p_unit1 = create(:parent)
 p_unit2 = create(:parent)
