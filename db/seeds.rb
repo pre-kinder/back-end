16c54e9eecbfec510117aec6cf73bfd4183fe805
iPowers <isika.powers@gmail.com>
2021-11-11 15:01:32 -0700

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # require "factory_bot_rails"
-FactoryBot::Syntax::Methods
 
 Child.destroy_all
 Teacher.destroy_all
@@ -17,26 +16,75 @@ Classroom.destroy_all
 Parent.destroy_all
 
 
-class1 = create(:classroom)
-class2 = create(:classroom)
-class3 = create(:classroom)
+class1 = Classroom.create(name: "Room 1 Pintozzi Pumas")
+class2 = Classroom.create(name: "Room 2 Chipmunks")
+class3 = Classroom.create(name: "Room 3 Frogs")
 
-p_unit1 = create(:parent)
-p_unit2 = create(:parent)
-p_unit3 = create(:parent)
+def generate_childre
+  20.times do |i|
+    children = Children.create!(
+      first_name { Faker::Name.first_name }
+      last_name { Faker::Name.last_name }
+      birthday { Faker::Date.birthday(min_age: 3, max_age: 5) }
+    )
+  end
+  children
+end
 
-kid1 = create(:child, classroom: class1, parent: p_unit1)
-kid2 = create(:child, classroom: class1, parent: p_unit2)
-kid3 = create(:child, classroom: class2, parent: p_unit2)
-kid4 = create(:child, classroom: class2, parent: p_unit3)
 
-prof1 = create(:teacher, classroom: class1)
-prof2 = create(:teacher, classroom: class2)
-prof3 = create(:teacher, classroom: class2)
+p_unit1 = Parent.create(first_name: 'Lucy', last_name: 'Loo',
+                        phone_number: '123-455-6677',
+                        address: "123 Main St Denver, CO 37278",
+                        email: "lucy@gmail.com",
+                        google_id: '27494238520',
+                        google_image_url: '2368ihefndskzsaudfd89djfasf')
 
-occasion1 = create(:event, classroom: class1)
-occasion2 = create(:event, classroom: class2)
-occasion3 = create(:event, classroom: class2)
+p_unit2 = Parent.create(first_name: 'Dave', last_name: 'Doodle',
+                        phone_number: '362-348-3743',
+                        address: "123 Main St Denver, CO 37278",
+                        email: "dave@gmail.com",
+                        google_id: '3829440344',
+                        google_image_url: '2368ihefndskzsaudfd89djfasf')
+
+p_unit3 = Parent.create(first_name: 'Andy', last_name: 'Anty',
+                        phone_number: '363-522-4523',
+                        address: "123 Main St Denver, CO 37278",
+                        email: "andy@gmail.com",
+                        google_id: '634823749',
+                        google_image_url: '2368ihefndskzsaudfd89djfasf')
+
+# kid1 = create(:child, classroom: class1, parent: p_unit1)
+# kid2 = create(:child, classroom: class1, parent: p_unit2)
+# kid3 = create(:child, classroom: class2, parent: p_unit2)
+# kid4 = create(:child, classroom: class2, parent: p_unit3)
+
+teacher1 = Teacher.create(first_name: 'Amada', last_name: 'Aooooyooo',
+                        phone_number: '327-433-4343',
+                        address: "123 Main St Denver, CO 37278",
+                        email: "amada@gmail.com",
+                        google_id: '372349473',
+                        google_image_url: '2368ihefndskzsaudfd89djfasf',
+                        classroom: class1)
+
+teacher2 = Teacher.create(first_name: 'Betty', last_name: 'Tochihara',
+                      phone_number: '362-238-2384',
+                      address: "123 Main St Denver, CO 37278",
+                      email: "betty@gmail.com",
+                      google_id: '382947340123',
+                      google_image_url: '2368ihefndskzsaudfd89djfasf',
+                      classroom: class2)
+
+teacher3 = Teacher.create(first_name: 'Henry', last_name: 'Howdy',
+                      phone_number: '632-344-3432',
+                      address: "123 Main St Denver, CO 37278",
+                      email: "henry@gmail.com",
+                      google_id: '80034723',
+                      google_image_url: '2368ihefndskzsaudfd89djfasf',
+                      classroom: class3)
+
+# occasion1 = create(:event, classroom: class1)
+# occasion2 = create(:event, classroom: class2)
+# occasion3 = create(:event, classroom: class2)
 
 # post = create(:post, user: user)
 # class Seed
@@ -45,17 +93,6 @@ occasion3 = create(:event, classroom: class2)
 #     seed.generate_destinations
 #   end
 #
-#   def generate_destinations
-#     50.times do |i|
-#       dest = Destination.create!(
-#         name: Faker::Address.city,
-#         zip: Faker::Address.zip,
-#         description: Faker::Lorem.sentence,
-#         image_url: Faker::Placeholdit.image
-#       )
-#       puts "Destination #{i}: #{dest.name} created!"
-#     end
-#   end
-# end
+
 #
 # Seed.start
