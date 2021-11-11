66a1478be49531a9c7d63989c1cf7434daf0cc7c
iPowers <isika.powers@gmail.com>
2021-11-11 14:45:39 -0700

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,9 @@ Classroom.destroy_all
 Parent.destroy_all
 
 
-class1 = create(name: "Zoom 1 Pintozzi Pumas")
-class2 = create(name: "Room 2 Chipmunks")
-class3 = create(name: "Room 3 Frogs")
+class1 = create(:classroom)
+class2 = create(:classroom)
+class3 = create(:classroom)
 
 
 p_unit1 = create(:parent)
